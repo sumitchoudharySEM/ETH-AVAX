2935aba1f634b5760752df781cd1f9c77db64bba
Sumit Choudhary <97527022+sumitchoudharySEM@users.noreply.github.com>
2023-05-23 16:07:13 +0530

    Update errorhandling.sol

--- errorhandling.sol
+++ errorhandling.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract NumberOperations {
+    uint public  number;
+    
+    function setNumber(uint newValue) external {
+        require(newValue > 0, "Number must be greater than zero");
+        
+        number = newValue;
+    }
+    
+    function add(uint value) external view returns (uint) {
+        assert(value == 4);
+        
+        return number + value;
+    }
+    
+    function subtract(uint value) external view returns (uint) {
+
+        if (value < 5) {
+            revert("Input must be less than 5");
+        }
+        return number - value;
+    }
+}
 
